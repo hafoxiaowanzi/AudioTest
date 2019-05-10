d4a08097a826180d9e218807df2644a38c5f505e
hafoxiaowanzi <wangwanxin123@163.com>
2019-05-10 15:19:05 +0800

    修改playbackCallbackNoblock.py中语音默认参数为config文件加中的配置值

--- Record/PlaybackCallbackNoblock.py
+++ Record/PlaybackCallbackNoblock.py
@@ -15,19 +15,20 @@ This is the callback (non-blocking) version.
 
 import pyaudio
 import time
+import config.RecordConfig as config
 
-WIDTH = 2
-CHANNELS = 2
-RATE = 44100
+# WIDTH = 2
+# CHANNELS = 2
+# RATE = 44100
 
 p = pyaudio.PyAudio()
 
 def callback(in_data, frame_count, time_info, status):
     return (in_data, pyaudio.paContinue)
 
-stream = p.open(format=p.get_format_from_width(WIDTH),
-                channels=CHANNELS,
-                rate=RATE,
+stream = p.open(format=p.get_format_from_width(config.WIDTH),
+                channels=config.CHANNELS,
+                rate=config.RATE,
                 input=True,
                 output=True,
                 stream_callback=callback)
